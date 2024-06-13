50b171ab930c5704d7866c7ad207e5837d97d71a
Gaurav Sharma <sharmag8121@gmail.com>
2024-06-13 15:48:37 +0530

    Handling POST Requests

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+app.use(express.json());
+
 /*
 //Defining route
 app.get('/', (req, res) => {
@@ -24,6 +26,24 @@ app.get('/api/v1/tours', (req, res) => {
         }
      })
 });
+
+
+app.post('/api/v1/tours', (req, res) => {
+    console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body)
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+    });
+    // res.send('Done');
+});
+
 
 // Starting a server
 const port = 3000;
