720b7e0c35b11207e613ab8a229f40532a63bf6d
Gaurav Sharma <sharmag8121@gmail.com>
2024-08-04 03:20:10 +0530

    Responding to URL Parameters

--- app.js
+++ app.js
@@ -19,20 +19,41 @@ const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simpl
 
 // Starting Our API Handling GET Request
 app.get('/api/v1/tours', (req, res) => {
-     res.status(200).json({
+    res.status(200).json({
         status: 'success',
         result: tours.length,
         data: {
             tours
         }
-     })
+    })
 });
 
+// Responding to URL Parameters
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+    
+    //if (id > tours.length) 
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+   
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+});
 
 app.post('/api/v1/tours', (req, res) => {
     console.log(req.body);
     const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({id: newId}, req.body)
+    const newTour = Object.assign({ id: newId }, req.body)
     tours.push(newTour);
     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
         res.status(201).json({
@@ -44,7 +65,6 @@ app.post('/api/v1/tours', (req, res) => {
     });
     // res.send('Done');
 });
-
 
 // Starting a server
 const port = 3000;
