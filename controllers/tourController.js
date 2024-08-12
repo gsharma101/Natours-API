43e67c32d3fddca70c10731cc9aa0427ba5eff78
Gaurav Sharma <sharmag8121@gmail.com>
2024-08-12 16:20:11 +0530

    Modelling the Tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -58,7 +58,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: 'Invalid data sent!',
+      message: err
     });
   }
 };
